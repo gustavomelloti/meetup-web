51026476551edb803d95f1687009179716570aa1
Gustavo Mello <gustavomelloti@gmail.com>
2019-10-12 07:41:31 -0300

    criando layout da tela de listagem de meetups

--- src/pages/Meetups/List/index.js
+++ src/pages/Meetups/List/index.js
@@ -1,7 +1,53 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Title,
+  MeetupBox,
+  Meetup,
+  MeetupName,
+  MeetupDate,
+} from './styles';
 
 export default function List() {
-  return <h1>LISTANDO</h1>;
+  return (
+    <Container>
+      <Title>
+        <span>Meus meetups</span>
+        <button type="button">
+          <i className="fa fa-plus-circle" />
+          Novo meetup
+        </button>
+      </Title>
+      <MeetupBox>
+        <Meetup>
+          <div>
+            <MeetupName>Meetup de React Native</MeetupName>
+          </div>
+          <div>
+            <MeetupDate>23 de junho, as 20h</MeetupDate>
+            <i className="fa fas fa-chevron-right" />
+          </div>
+        </Meetup>
+        <Meetup>
+          <div>
+            <MeetupName>Meetup de React Native</MeetupName>
+          </div>
+          <div>
+            <MeetupDate>23 de junho, as 20h</MeetupDate>
+            <i className="fa fas fa-chevron-right" />
+          </div>
+        </Meetup>
+        <Meetup>
+          <div>
+            <MeetupName>Node JS</MeetupName>
+          </div>
+          <div>
+            <MeetupDate>23 de junho, as 20h</MeetupDate>
+            <i className="fa fas fa-chevron-right" />
+          </div>
+        </Meetup>
+      </MeetupBox>
+    </Container>
+  );
 }
---
+++ src/pages/Meetups/List/styles.js
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+import { darken, lighten } from 'polished';
+
+export const Container = styled.div`
+  max-width: 940px;
+  margin: 50px auto;
+`;
+
+export const Title = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  span {
+    color: #fff;
+    font-size: 32px;
+    font-weight: bold;
+  }
+
+  button {
+    margin: 5px 0 0;
+    height: 42px;
+    width: 162px;
+    background: #f94d6a;
+    font-weight: bold;
+    color: #fff;
+    border: 0;
+    border-radius: 4px;
+    font-size: 18px;
+    transition: background 0.2s;
+
+    &:hover {
+      background: ${darken(0.03, '#f94d6a')};
+    }
+
+    i {
+      margin-right: 10px;
+      font-size: 20px;
+    }
+  }
+`;
+
+export const MeetupBox = styled.div`
+  margin-top: 100px;
+`;
+
+export const Meetup = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 62px;
+  background: ${darken(0.1, '#402845')};
+  margin-bottom: 10px;
+  border-radius: 5px;
+  padding: 0px 20px;
+  transition: background 0.2s;
+  cursor: pointer;
+
+  &:hover {
+    background: ${lighten(0.02, '#402845')};
+  }
+
+  div {
+    display: flex;
+
+    i {
+      color: #fff;
+    }
+  }
+`;
+
+export const MeetupName = styled.div`
+  color: #fff;
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+export const MeetupDate = styled.div`
+  color: #999;
+  font-size: 16px;
+  padding-right: 20px;
+`;
